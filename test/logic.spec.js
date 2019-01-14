f6a9fcf663ab8d226daf050e2514f6d565f7a885
AKSAHA <sheol.aks@gmail.com>
2019-01-14 11:22:21 +0530

    Added unt test

--- test/logic.spec.js
+++ test/logic.spec.js
@@ -15,4 +15,10 @@ describe('Logic file', function() {
         assert.equal(logic.jumpingOnCloudsEmma([0, 0, 0, 1,0, 0]), 3);
       });
     });
+
+    describe('cutTheSticks ', function() {
+      it('should return 4 results when ths [1, 2, 3, 4, 3, 3, 2, 1] value  present', function() {
+        assert.equal(logic.cutTheSticks([1, 2, 3, 4, 3, 3, 2, 1]).length, 4);
+      });
+    });
   });

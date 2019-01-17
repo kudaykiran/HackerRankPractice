0789ff1f8ffa56330a0048c466ad6023b6db1eef
AKSAHA <sheol.aks@gmail.com>
2019-01-17 12:04:49 +0530

    Refactored the code

--- logic.js
+++ logic.js
@@ -1,253 +1,44 @@
+var fiveStar = require("./fivestar");
+
 module.exports = {
-    foo: function (data) {
-        return 'from ' + data;
-    },
-
-    pangrams: function(s){
-        s = s.toLowerCase();
-        let alp = 'abcdefghijklmnopqrstuvwxyz'.split('');
-        let count = 1;
-        for(let i = 0;i<alp.length ; i++){
-            if(s.includes(alp[i])) {count++;}
-            else{ 
-                let v = alp[i];
-                v= '';
-            }
-        }
-
-        return count > 26 ? 'pangram' : 'not pangram';
-
+    pangrams: function(s){       
+        return fiveStar.pangrams(s);
     },
 
     hackerrankInString: function(s){
-        let mainString = 'hackerrank'.split('');
-        let isPossible = '';
-        let passedStrCharSet = s.split('');
-        let i =0;
-        while(passedStrCharSet.length > 0){
-            let temp = mainString[i];
-            let temp1 = passedStrCharSet.join('');
-            if(passedStrCharSet.findIndex(x => x == mainString[i]) > -1){
-                
-                let ind = passedStrCharSet.findIndex(x => x == mainString[i]);
-                passedStrCharSet = passedStrCharSet.slice(ind == 0 ? 1 : ind + 1, passedStrCharSet.length);
-
-                i++;
-            } else {
-                break;
-            }
-            
-            
-        }
-        if(i == mainString.length){
-            isPossible  = "YES";
-        } else {
-            isPossible = 'NO';
-        }
-        
-        return isPossible;
+        return fiveStar.hackerrankInString(s);
     },
 
     marsExploration : function(s) {
-        let exceptSOS = s.split('').filter(x => x != 'SOS');
-        let changed = 0;
-        for(let i = 0; i<exceptSOS.length ; i= i+3){
-            if(exceptSOS[i] != 'S'){
-                changed++;
-            }
-            if(exceptSOS[i + 1] != 'O'){
-                changed++;
-            }
-            if(exceptSOS[i + 2] != 'S'){
-                changed++;
-            }
-        }
-
-        return changed;
-    
+        return fiveStar.marsExploration(s);    
     },
 
     camelcase : function(s){
         let numOfWords = s.replace(/[^A-Z]/g, "").length;
-
-        
         return numOfWords + 1;
     },
 
-    timeInWords: function(h, m){
-        let single = ['','one','two','three','four','five','six','seven','eight','nine'];
-        let ten = ['ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' quarter', ' sixteen', ' seventeen', ' eighteen', ' nineteen','twenty'];
-
-        let retString = '';
-
-
-        if(m == 00){
-            retString += single[h] + ' o\' clock';
-        } else if(Number(m).toString().length == 1){
-            retString += single[m] + ' minute past '+single[h];
-        } else {
-            // Past
-            if(Number(m)<30){
-                if(Number(m)< 20){
-                    if(Number(m) == 15){
-                        retString += ten[Number(m.toString().slice(1,2))] + ' past '+single[h];
-                    } else {
-                    retString += ten[Number(m.toString().slice(1,2))] + ' minute past '+single[h];
-                    }
-                } else {
-                    retString += 'twenty ' + single[Number(m.toString().slice(1,2))] + ' minutes past '+single[h];
-                }
-            } // To
-             else if(Number(m)>30){
-                if(60 - Number(m)< 9){
-                    retString +=  single[Number((60 - Number(m)).toString())] + ' minutes to';
-                    if(single[h + 1] != undefined){
-                        retString +=  single[h + 1];
-                    } else {
-                        retString += ten[Number(h) - 10 + 1];
-                    }
-
-                } else {
-                    if(60 - Number(m) > 9 && 60 - Number(m) < 20){
-                        if(60 - Number(m) == 15){
-                            retString += ten[Number((60 - Number(m)).toString().slice(1,2))] + ' to '+single[h + 1];
-                        } else {
-                        //retString += ten[Number(m.toString().slice(1,2))] + ' minute past '+single[h];
-                        retString += ten[Number((60 - Number(m)).toString().slice(1,2))] + ' minutes to '+single[h + 1];
-
-                        }
-
-                    } else {
-                        retString += 'twenty ' + single[Number((60 - Number(m)).toString().slice(1,2))] + ' minutes to '+single[h + 1];
-
-                    }
-                }
-
-            } // For 30 
-             else {
-                retString += 'half past '+single[h];
-            }
-        }
-
-        return retString.trim();
+    timeInWords: function(h, m){       
+        return fiveStar.timeInWords(h,m);
     },
 
     funnyString: function (s){
-        let mainString = s.split('');
-        let reverseChar = s.split('').reverse();
-        let ansciValueOfString = [];
-        let ansciValueOfReverseString = [];
-
-        ansciValueOfString.push(
-
-            mainString.map((x) => x.charCodeAt(0))
-            
-        );
-
-        ansciValueOfReverseString.push(
-
-            reverseChar.map((x) => x.charCodeAt(0))
-            
-        );
-
-        let isFunnyOrNot = '';
-
-         
-        for(let i =0; i<ansciValueOfString[0].length; i++){
-            let c1=  Math.abs(ansciValueOfString[0][i + 1] - ansciValueOfString[0][i]);
-            let c2= Math.abs (ansciValueOfReverseString[0][i + 1] - ansciValueOfReverseString[0][i]);
-            
-            if(ansciValueOfString[0][i + 1]  == undefined) {
-                return isFunnyOrNot == '' ? 'Not Funny' :  isFunnyOrNot;
-            } else if( Math.abs(ansciValueOfString[0][i + 1] - ansciValueOfString[0][i])
-                  != Math.abs(ansciValueOfReverseString[0][i + 1] - ansciValueOfReverseString[0][i])){
-                    isFunnyOrNot = 'Not Funny';
-                    break;
-            } else {
-                isFunnyOrNot = 'Funny';
-            }
-        }
-
-        return isFunnyOrNot;
+       
+        return fiveStar.funnyString(s);
     },
 
     minimumDistances : function(a) {
-        let minimumDist = a.length;
-        for(let i= 0; i< a.length; i++){
-            for(let j = a.length - 1; j> i; j--){
-                let k= a[i];
-                let n=a[j];
-                if(a[i] == a[j] && (j - i)< minimumDist){
-                    minimumDist = j-i;
-                    break;
-                }
-            }
-        }
-        return minimumDist == a.length ? -1 : minimumDist;
+        return fiveStar.minimumDistances(s);
     },
 
     jumpingOnCloudsEmma: function (c) {
-        if (c.every(x => x == 0)) {
-            return 1;
-        }
-
-        let numOfJumps = 0;
-        let startingPoint = 0;
-        for (let i = 0; i < c.length; i++) {
-            if(i == 0 && c[i] == 0){
-                startingPoint = 0;
-                continue;
-            }
-            if(i == startingPoint + 2 && c[i] == 0){
-                startingPoint = i;
-                numOfJumps++;
-            } else if(c[i] == 0 && c[i+1] != undefined && c[i+1] == 1){
-                startingPoint = i;
-                numOfJumps++;
-            } else {
-                numOfJumps = numOfJumps;
-            }
-            if(c[i + 1] == undefined && (startingPoint <= i -1 || startingPoint <= i -2) ){
-                numOfJumps++;
-            }
-        }
-
-        return numOfJumps;
+        
+        return fiveStar.jumpingOnCloudsEmma(c);
     },
 
     jumpingOnClouds: function(c, k){
         
-        let energy =100;
-
-        if(c == k){
-            if(c[0] == 1){
-                energy =energy -3;
-            }
-        } else {
-
-            for(let i =0;i<c.length;i++){
-                let afterJumpClud = i + k;
-                energy--;
-                if(c[afterJumpClud] == 1){
-                    energy =energy -2;
-                }
-                i = afterJumpClud - 1;
-            }
-
-            if(c.length % k != 0){
-                if(c[0] == 1){
-                    energy = energy -2;
-                } else{
-                    energy--;
-                }
-            }
-            if(c.every( x => x == 1)){
-                energy = 100 - c.length * 3;
-            }
-            
-        }
-
-        return energy
+        return fiveStar.jumpingOnClouds(c,k)
 
     },
 

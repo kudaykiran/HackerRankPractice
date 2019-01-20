c510256c99f43adf12eae190bf0629d22c6f1886
AKSAHA <sheol.aks@gmail.com>
2019-01-20 14:06:27 +0530

    Rolled back changes

--- fourstar.js
+++ fourstar.js
@@ -117,7 +117,22 @@ module.exports ={
         let valleys = 0;
 
         s = s.split('');
-        valleys = utils.getVallies(s,currLevel,valleys);
+        //valleys = utils.getVallies(s,currLevel,valleys);
+
+        getVallies: function(s,currLevel,valleys){
+            for (let i = 0; i < s.length; i++) {
+                //update the current level
+                if (s[i] === 'U') {
+                    currLevel += 1;
+                    if (currLevel == 0) {
+                        valleys += 1;
+                    }
+                } else {
+                    currLevel -= 1;
+                }
+            }
+            return valleys;
+        },
 
         return valleys;
     },
